2acc7220c755efc579df207e71f43ce24dda2544
Jebakani <vjishwarya@gmail.com>
2021-08-04 22:09:11 +0530

    [JEBAKANI] Adding 25 review list the gives product review

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -7,6 +7,8 @@ namespace ProductReviewManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Management System");
+            ReviewManager review = new ReviewManager();
+            review.AddReviews();
         }
     }
 }
